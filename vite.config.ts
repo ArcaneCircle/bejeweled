105329aaddd8dec107252fb50d8af5904792b184
M4ss1ck <4ndyraul@gmail.com>
2022-08-02 10:12:18 -0400

    remove react

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@
 
 import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import AutoImport from 'unplugin-auto-import/vite'
-import Unocss from 'unocss/vite'
 
 export default defineConfig({
   resolve: {
@@ -12,20 +9,7 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [
-    react(),
-    Unocss(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'react',
-        'react-router-dom',
-        { 'usehooks-ts': ['useCounter', 'useDarkMode'] },
-      ],
-      dts: true,
-    }),
-  ],
+  plugins: [],
 
   // https://github.com/vitest-dev/vitest
   test: {
